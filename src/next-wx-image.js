93429d0d4c4bb62152fa8ab96dba81e5df13faa8
afeiship <1290657123@qq.com>
2018-10-12 16:35:10 +0800

    issue: context fix

--- src/next-wx-image.js
+++ src/next-wx-image.js
@@ -1,6 +1,6 @@
 (function () {
 
-  var global = global || this || self || window;
+  var global = global || this || window || Function("return this")();
   var nx = global.nx || require('next-js-core2');
   var wx = global.wx = global.wx;
 
